254823a834068554916824577593df696cbbdc34
Adrian Melo <adrianmelo1390995@gmail.com>
2023-11-27 18:34:03 -0600

    Updated to try and run with pygbag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pygame
-from planet import Planet
+import asyncio
+from planet import *
 
 pygame.init()
 
@@ -15,6 +15,31 @@ DARK_GREY = (80, 78, 81)
 
 FONT = pygame.font.SysFont("TimesNewRoman", 16)
 
+clock = pygame.time.Clock()
+
+sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10 ** 30, "Sun")
+sun.sun = True
+earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10 ** 24, "Earth")
+moon_distance_from_earth = 384.4e3 * Planet.KM_TO_MILES / Planet.SCALE
+moon = Planet(earth.pos[0] - moon_distance_from_earth, 0, 4, (128, 128, 128), 7.342 * 10 ** 22, "Moon")
+mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10 ** 23, "Mars")
+mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10 ** 23, "Mercury")
+venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10 ** 24, "Venus")
+
+# Initialize last year for text surface caching
+for planet in [earth, moon, mars, mercury, venus]:
+    planet.last_year = 0
+
+# Assigning initial velocities to the planets
+earth.vel[1] = 29.783 * 1000
+mars.vel[1] = 24.077 * 1000
+mercury.vel[1] = -47.4 * 1000
+venus.vel[1] = -35.02 * 1000
+moon.vel[1] = earth.vel[1] + 1.022 * 1000
+
+planets = [sun, mercury, venus, earth, moon, mars]
+
+elapsed_time_seconds = 0
 
 def draw_key(win, planets):
     start_x = WIDTH - 200
@@ -22,22 +47,18 @@ def draw_key(win, planets):
     padding = 20
 
     for planet in planets:
-        pygame.draw.circle(win, planet.color, (start_x, start_y), 10)  # Draw a small circle of the planet's color
+        pygame.draw.circle(win, planet.color, (start_x, start_y), 10)
         name_text = FONT.render(planet.name, 1, WHITE)
-        win.blit(name_text, (start_x + 15, start_y - 5))  # Draw the planet's name next to the circle
-        start_y += padding  # Move down for the next entry
-
+        win.blit(name_text, (start_x + 15, start_y - 5))
+        start_y += padding
 
 def draw_year_counter(win, elapsed_time_seconds, planets, font, white):
-    # Calculate years elapsed for all planets
     years_elapsed = {
         "Earth": elapsed_time_seconds / (365.25 * 24 * 3600),
         "Mercury": elapsed_time_seconds / (88 * 24 * 3600),
         "Venus": elapsed_time_seconds / (224.7 * 24 * 3600),
         "Mars": elapsed_time_seconds / (687 * 24 * 3600)
     }
-
-    # Define the desired order of planet names
     planet_order = ["Mars", "Earth", "Venus", "Mercury"]
 
     start_y = HEIGHT - 10 - font.get_height()
@@ -46,58 +67,34 @@ def draw_year_counter(win, elapsed_time_seconds, planets, font, white):
     for planet_name in planet_order:
         planet = next((p for p in planets if p.name == planet_name), None)
         if planet:
-            text = font.render(f"Years on {planet.name}: {years_elapsed[planet.name]:.2f}", 1, white)
-            win.blit(text, (10, start_y))
+            # Update text surface only if necessary
+            if 'text_surface' not in planet.__dict__ or int(years_elapsed[planet.name]) != int(planet.last_year):
+                planet.text_surface = font.render(f"Years on {planet.name}: {years_elapsed[planet.name]:.2f}", 1, white)
+                planet.last_year = years_elapsed[planet.name]
+            win.blit(planet.text_surface, (10, start_y))
             start_y -= padding
 
+async def main():
+    global elapsed_time_seconds
 
-def main():
-    run = True
-    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return  # Exit the program
 
-    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10 ** 30, "Sun")
-    sun.sun = True
-
-    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10 ** 24, "Earth")
-
-    moon_distance_from_earth = 384.4e3 * Planet.KM_TO_MILES / Planet.SCALE  # Convert km to miles and scale
-    moon = Planet(earth.pos[0] - moon_distance_from_earth, 0, 4, (128, 128, 128), 7.342 * 10 ** 22, "Moon")
-
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10 ** 23, "Mars")
-    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10 ** 23, "Mercury")
-    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10 ** 24, "Venus")
-
-    # Assigning initial velocities to the planets
-    earth.vel[1] = 29.783 * 1000
-    mars.vel[1] = 24.077 * 1000
-    mercury.vel[1] = -47.4 * 1000
-    venus.vel[1] = -35.02 * 1000
-
-    moon.vel[1] = earth.vel[1] + 1.022 * 1000
-
-    planets = [sun, mercury, venus, earth, moon, mars]
-
-    elapsed_time_seconds = 0
-
-    while run:
-        clock.tick(60)
+        clock.tick(30)  # Reduced frame rate for optimization
         elapsed_time_seconds += Planet.TIMESTEP
         WIN.fill((0, 0, 0))
 
         draw_year_counter(WIN, elapsed_time_seconds, planets, FONT, WHITE)
-
         draw_key(WIN, planets)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
 
         for planet in planets:
             planet.update_position(planets)
             planet.draw(WIN, WIDTH, HEIGHT, FONT, WHITE)
 
         pygame.display.update()
+        await asyncio.sleep(0)
 
-    pygame.quit()
-
-
-main()
+asyncio.run(main())
--- planet.py
+++ planet.py
@@ -1,14 +1,15 @@
-import pygame
 import numpy as np
-
+import pygame
 
 class Planet:
     KM_TO_MILES = 0.621371  # Conversion factor from kilometers to miles
     AU_IN_KM = 149.6e6 * 1000  # Astronomical Unit (AU) in kilometers
     AU = AU_IN_KM * KM_TO_MILES  # Astronomical Unit (AU) in miles
-    G = 6.67428e-11  # gravitational constant
-    SCALE = 250 / AU  # scaling for the program
-    TIMESTEP = 3600 * 24 * 0.5 # time step for the simulation in seconds
+    G = 6.67428e-11  # Gravitational constant
+    SCALE = 250 / AU  # Scaling for the program
+    TIMESTEP = 3600 * 24 * 0.5  # Time step for the simulation in seconds
+    MAX_ORBIT_LENGTH = 100  # Maximum number of positions to store in the orbit
+    DISTANCE_UPDATE_INTERVAL = 100  # Update distance every 100 frames
 
     def __init__(self, x, y, radius, color, mass, name=None):
         self.pos = np.array([x, y], dtype=float)
@@ -20,9 +21,11 @@ class Planet:
         self.distance_to_sun = 0
         self.vel = np.array([0.0, 0.0])
         self.name = name
+        self.frame_counter = 0
 
         # Create an empty surface with transparency for the orbit
         self.orbit_surface = pygame.Surface((1200, 800), pygame.SRCALPHA)
+        self.distance_text_surface = None  # Surface for distance text
 
     def draw(self, win, width, height, font, white):
         x, y = (self.pos * self.SCALE + np.array([width / 2, height / 2])).astype(int)
@@ -30,9 +33,10 @@ class Planet:
         # Draw the orbit
         if len(self.orbit) > 2:
             for i in range(1, len(self.orbit)):
-                start_point = (
-                    self.orbit[i - 1][0] * self.SCALE + width / 2, self.orbit[i - 1][1] * self.SCALE + height / 2)
-                end_point = (self.orbit[i][0] * self.SCALE + width / 2, self.orbit[i][1] * self.SCALE + height / 2)
+                start_point = (self.orbit[i - 1][0] * self.SCALE + width / 2,
+                               self.orbit[i - 1][1] * self.SCALE + height / 2)
+                end_point = (self.orbit[i][0] * self.SCALE + width / 2,
+                             self.orbit[i][1] * self.SCALE + height / 2)
 
                 fade_multiplier = 15  # Increase this for faster fading
                 fade_value = int(255 * (i / len(self.orbit)) ** fade_multiplier)
@@ -44,8 +48,9 @@ class Planet:
         pygame.draw.circle(win, self.color, (x, y), self.radius)
 
         if not self.sun:
-            distance_text = font.render(f"{round(self.distance_to_sun, 1):,} miles", 1, white)
-            win.blit(distance_text, (x + self.radius + 5, y - distance_text.get_height() / 2))
+            if self.distance_text_surface is None:  # Create the distance text surface if it doesn't exist
+                self.distance_text_surface = font.render(f"{self.distance_to_sun:e} miles", 1, white)
+            win.blit(self.distance_text_surface, (x + self.radius + 5, y - self.distance_text_surface.get_height() / 2))
 
     def attraction(self, other):
         distance_vec = other.pos - self.pos
@@ -68,4 +73,15 @@ class Planet:
         acc = total_force / self.mass
         self.vel += acc * self.TIMESTEP
         self.pos += self.vel * self.TIMESTEP
+
+        # Update the orbit and limit its length
         self.orbit.append(tuple(self.pos))
+        if len(self.orbit) > self.MAX_ORBIT_LENGTH:
+            self.orbit.pop(0)  # Remove the oldest position if orbit exceeds maximum length
+
+        # Update distance to sun at specified intervals
+        self.frame_counter += 1
+        if self.frame_counter >= self.DISTANCE_UPDATE_INTERVAL:
+            self.frame_counter = 0
+            if not self.sun:
+                self.distance_to_sun = np.linalg.norm(self.pos - planets[0].pos) * self.SCALE / self.KM_TO_MILES
